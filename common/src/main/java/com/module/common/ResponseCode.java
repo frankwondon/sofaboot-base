49d8e9d9e865e54dc26729525f48230c4bd842a2
frankwondon <frankwondon@qq.com>
2019-04-30 11:22:18 +0800

    jwt

--- common/src/main/java/com/module/common/ResponseCode.java
+++ common/src/main/java/com/module/common/ResponseCode.java
@@ -2,11 +2,11 @@ package com.module.common;
 
 
 public enum  ResponseCode {
-    C_200(200,"请求成功");
+    C_200(200,"请求成功","request success");
     private int code;
     private String cnMsg;
     private String enMsg;
-    private  ResponseCode(int code,String cnMsg){
+    private  ResponseCode(int code,String cnMsg,String enMsg){
         this.code=code;
         this.cnMsg=cnMsg;
     }
