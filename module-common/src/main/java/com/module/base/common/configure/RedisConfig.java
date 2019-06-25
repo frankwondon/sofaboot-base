1d6b7563f101aa3529613f732898f50d52c1651f
frank <frankwondon@qq.com>
2019-06-25 22:27:19 +0800

    家中开发

--- module-common/src/main/java/com/module/base/common/configure/RedisConfig.java
+++ module-common/src/main/java/com/module/base/common/configure/RedisConfig.java
@@ -2,18 +2,29 @@ package com.module.base.common.configure;
 
 
 import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
 
 /**
  * @author wangdong
  * @date: 2019/6/25 18:45
  */
+@Configuration
+@EnableConfigurationProperties
+@ConditionalOnProperty(prefix = "unit.redis",name = "enable",havingValue = "true")
 public class RedisConfig {
-
+  @Resource
+  private RedisConfigProperties redisConfigProperties;
   private Config config(){
     Config config=new Config();
-    config.useClusterServers()
-            // use "rediss://" for SSL connection
-            .addNodeAddress("redis://127.0.0.1:7181");
+    config.useSingleServer().setAddress(redisConfigProperties.getAddress())
+            .setDatabase(redisConfigProperties.getDbNum())
+            .setConnectionPoolSize(20)
+            .setConnectTimeout(redisConfigProperties.getTimeOut())
+            .setPassword(redisConfigProperties.getPassWord());
     config.setThreads(20);
     return config;
   }
--- module-common/src/main/java/com/module/base/common/configure/RedisConfigProperties.java
+++ module-common/src/main/java/com/module/base/common/configure/RedisConfigProperties.java
@@ -1,11 +1,21 @@
 package com.module.base.common.configure;
 
 
+import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 /**
  * @author wangdong
  * @date: 2019/6/25 18:45
  */
+@ConfigurationProperties(prefix = "unit.redis")
+
+@Data
 public class RedisConfigProperties {
-
-
+        private Boolean enable;
+        private String address;
+        private Integer dbNum;
+        private String passWord;
+        private Integer  timeOut;
 }
